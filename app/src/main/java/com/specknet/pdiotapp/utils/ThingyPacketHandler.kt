9677a435c7daeaa9aa5dc30fedf832b9e6c37925
Nikita Rameshkumar <nikitaramesh523@gmail.com>
2024-11-18 18:17:18 +0000

    Reset to working version

--- app/src/main/java/com/specknet/pdiotapp/utils/ThingyPacketHandler.kt
+++ app/src/main/java/com/specknet/pdiotapp/utils/ThingyPacketHandler.kt
@@ -5,9 +5,9 @@ import android.util.Log
 import com.specknet.pdiotapp.bluetooth.BluetoothSpeckService
 
 /**
- * This class processes new RESpeck packets which are passed from the SpeckBluetoothService.
+ * This class processes new Thingy packets which are passed from the SpeckBluetoothService.
  * It contains all logic to transform the incoming bytes into the desired variables and then stores and broadcasts
- * this information
+ * this information.
  */
 class ThingyPacketHandler(val speckService: BluetoothSpeckService) {
 
@@ -18,29 +18,43 @@ class ThingyPacketHandler(val speckService: BluetoothSpeckService) {
             field = value
         }
 
-    // TODO here is where we process a thingy packet
+    // Buffer for sliding window (if needed)
+    private val thingyBuffer: ArrayDeque<FloatArray> = ArrayDeque()
+
+    // Process a Thingy packet
     fun processThingyPacket(values: ByteArray) {
         val actualPhoneTimestamp = Utils.getUnixTimestamp()
 
+        // Decode the Thingy packet
         val thingyData = ThingyPacketDecoder.decodeThingyPacket(values)
 
-        Log.d(TAG, "processThingyPacket: decoded data " + thingyData.toString())
-        
-        // TODO only one sample per batch here
+        Log.d(TAG, "processThingyPacket: decoded data $thingyData")
+
+        // Preprocess accelerometer data (normalize by dividing by 9.81 to convert to G-forces)
+        val accelX = thingyData.accelData.x / 9.81f
+        val accelY = thingyData.accelData.y / 9.81f
+        val accelZ = thingyData.accelData.z / 9.81f
+
+        // Add new sample to buffer (if you're using sliding windows)
+        thingyBuffer.add(floatArrayOf(accelX, accelY, accelZ))
+
+        // You can apply sliding window processing here if needed (e.g., after collecting enough samples)
+
+        // Create new ThingyLiveData object with preprocessed values
         val newThingyLiveData = ThingyLiveData(
             actualPhoneTimestamp,
-            thingyData.accelData.x,
-            thingyData.accelData.y,
-            thingyData.accelData.z,
-            thingyData.gyroData,
-            thingyData.magData
+            accelX,
+            accelY,
+            accelZ,
+            thingyData.gyroData,  // Gyroscope data (already decoded)
+            thingyData.magData     // Magnetometer data (already decoded)
         )
+
         Log.i("Freq", "newThingyLiveData = $newThingyLiveData")
 
-        // Send live broadcast intent
+        // Send live broadcast intent with preprocessed data
         val liveDataIntent = Intent(Constants.ACTION_THINGY_BROADCAST)
         liveDataIntent.putExtra(Constants.THINGY_LIVE_DATA, newThingyLiveData)
         speckService.sendBroadcast(liveDataIntent)
     }
-        
 }
