7e5a40d33e426dc597ced7d17a72e0d81a8c29e7
isekind <s2030860@ed.ac.uk>
2024-11-19 16:55:42 +0000

    redo half a commit: add an activity summary/history page + necessary adjustments tin the database classes

--- app/src/main/java/com/specknet/pdiotapp/database/dao/ActivityLogDao.kt
+++ app/src/main/java/com/specknet/pdiotapp/database/dao/ActivityLogDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.specknet.pdiotapp.database.entities.ActivityLogEntry
+import com.specknet.pdiotapp.database.entities.ActivitySummary
 
 @Dao
 interface ActivityLogDao {
@@ -17,4 +18,13 @@ interface ActivityLogDao {
 
     @Query("SELECT * FROM activity_log WHERE date(startTime / 1000, 'unixepoch') = date('now')")
     fun getTodayEntries(): List<ActivityLogEntry>
+
+    @Query("""
+    SELECT activityName, SUM(endTime - startTime) as totalDuration
+    FROM activity_log
+    WHERE date(startTime / 1000, 'unixepoch', 'localtime') = :date
+    GROUP BY activityName
+""")
+    fun getActivitySummaryForDate(date: String): List<ActivitySummary>
+
 }
